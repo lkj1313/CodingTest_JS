3d648220eed6b619aef497ea9e882a246b1d05c4
lkj1313 <108466769+lkj1313@users.noreply.github.com>
2024-10-11 17:48:23 +0900

    [Bronze V] Title: ??!, Time: 92 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+
+// 입력을 받아서 처리
+const input = fs.readFileSync("/dev/stdin").toString().trim();
+
+// A와 B를 정수로 변환
+// const A = parseInt(input[0]);
+
+// 각 연산 결과를 출력
+console.log(input + "??!");
