1a192ed55d3cf50f83ed87a66aa4402f89b9e619
lkj1313 <108466769+lkj1313@users.noreply.github.com>
2024-10-12 13:39:39 +0900

    [Bronze V] Title: A+B - 8, Time: 88 ms, Memory: 9388 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+const T = +input[0];
+let result = "";
+
+for (i = 1; i <= T; i++) {
+  const [a, b] = input[i].split(" ").map(Number);
+  result += `Case #${i}: ${a} + ${b} = ${a + b} \n`;
+}
+
+console.log(result);
