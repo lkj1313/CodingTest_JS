d214dcc81ed2644544702507f40b4abc05355a82
lkj1313 <108466769+lkj1313@users.noreply.github.com>
2024-10-13 18:50:07 +0900

    [level 0] Title: 카운트 업, Time: 0.05 ms, Memory: 32.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.js
@@ -0,0 +1,10 @@
+function solution(start_num, end_num) {
+    const array = []
+    for(let i = start_num;  i<=end_num; i++){
+        array.push(i)
+    }
+    
+    return array
+}
+
+//end_nmu-start_num-1
