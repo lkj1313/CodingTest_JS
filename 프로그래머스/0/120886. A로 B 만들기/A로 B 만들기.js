451e99fa0f1a7aea3fd1b7835463ef392ff8fb52
lkj1313 <108466769+lkj1313@users.noreply.github.com>
2024-11-15 17:51:09 +0900

    [level 0] Title: A로 B 만들기, Time: 0.48 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.js
@@ -0,0 +1,5 @@
+function solution(before, after) {
+    const sortedBefore = before.split('').sort().join('');
+    const sortedAfter = after.split('').sort().join('');
+    return sortedBefore === sortedAfter ? 1 : 0;
+}
