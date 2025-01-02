ba44df0770a9aa5ee1c8e9241759528b7cb87d72
lkj1313 <108466769+lkj1313@users.noreply.github.com>
2025-01-02 15:52:08 +0900

    [level 1] Title: 문자열을 정수로 바꾸기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.js
@@ -0,0 +1,16 @@
+function solution(s) {
+    // var answer = 0;
+    // const arrayedNumber = s.split('');
+    // const number = arrayedNumber.map((n)=>{
+    //     if(n=== '-' || n=== '+'){
+    //         return n
+    //     } else{
+    //         return Number(n)
+    //     }
+    // })
+    // answer = number.join('')
+    // const ranswer = Number(answer)
+    // return ranswer;
+    let answer = Number(s)
+    return answer
+}
