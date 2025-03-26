01d5b1f563040025d779dc50a2ac70cad3057a80
lkj1313 <108466769+lkj1313@users.noreply.github.com>
2025-03-26 19:24:17 +0900

    [level 1] Title: 행렬의 덧셈, Time: 8.96 ms, Memory: 67.4 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
@@ -1,11 +1,5 @@
 function solution(arr1, arr2) {
-    var answer = [];
-    for(i=0; i<arr1.length; i++){
-        answer.push([]); // 각 행에 빈 배열을 먼저 추가
-        for(j=0; j<arr1[i].length; j++){
-            const sum = arr1[i][j] + arr2[i][j]
-            answer[i].push(sum)
-        }
-    }
-    return answer;
+    var answer = [[]];
+   const sum = arr1.map((row,i)=>row.map((val,j)=>val+arr2[i][j]))
+    return sum;
 }
