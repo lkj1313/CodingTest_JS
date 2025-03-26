4dc7577646424fe514ad05d4564c2a41e5dbb620
lkj1313 <108466769+lkj1313@users.noreply.github.com>
2025-03-26 19:54:18 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.16 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -1,34 +1,11 @@
-function solution(n, m) {
-    // 각 숫자의 약수를 먼저 구하고 -> 동일한 값을 뽑아낸 뒤에 -> 그중에 가장 큰 값
-    // 두 수의 곱을 최대공약수로 나눈 값 = 최소공배수
-    let divisorsN = []; // n의 약수
-    let divisorsM = []; // m의 약수
-    
-    // n의 약수 구하기
-    for (let i = 1; i <= n; i++) {
-        if (n % i === 0) {
-            divisorsN.push(i);
+function solution(n,m){
+    let gcd = 1; // 최대공약수
+    const min_num = Math.min(n,m)
+    for(let i=1; i<=min_num; i++ ){
+        if(n%i===0 && m%i===0){
+            gcd = i
         }
     }
-
-    // m의 약수 구하기
-    for (let i = 1; i <= m; i++) {
-        if (m % i === 0) {
-            divisorsM.push(i);
-        }
-    }
-    
-    // 공약수 배열을 구하기
-    const commonDivisors = divisorsN.filter(item => divisorsM.includes(item));
-    const greatestCommonDivisor = Math.max(...commonDivisors);
-    
-    // 최소공배수 구하기
-    const leastCommonMultiple = (n * m) / greatestCommonDivisor;
-    
-    // 결과 배열에 추가
-    let answer = []; 
-    answer.push(greatestCommonDivisor);
-    answer.push(leastCommonMultiple);
-    
-    return answer;
+    let 최소공배수 = n*m/gcd
+    return [gcd, 최소공배수]
 }
