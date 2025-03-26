e1ab2222b1735e31391cd2b6ebda69b74a193b78
lkj1313 <108466769+lkj1313@users.noreply.github.com>
2025-03-26 20:15:35 +0900

    [level 1] Title: 예산, Time: 0.22 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.js
+++ 프로그래머스/1/12982. 예산/예산.js
@@ -1,15 +1,13 @@
 function solution(d, budget) {
-    
-    // 몇개의 부서에 지원했는지 리턴하라
-    var answer = 0;
-    const 오름차순 = d.sort((a,b)=> a-b);
-    let 오름차순으로더한결과 = 0;
-    let array = [];
-    for(let i=0; i<d.length; i++){
-        if(오름차순으로더한결과<budget && 오름차순[i]+오름차순으로더한결과<=budget){
-            오름차순으로더한결과+= 오름차순[i];
-            array.push(오름차순[i])
-        }
+    let sum = 0;
+    let answer = 0;
+    const sorted_array = d.sort((a,b)=>a - b)
+    for(let i = 0; i<sorted_array.length ;i++){
+        sum+= sorted_array[i]
+        if(sum>budget) break
+        answer++
+        
     }
-    return array.length;
+    
+    return answer;
 }
