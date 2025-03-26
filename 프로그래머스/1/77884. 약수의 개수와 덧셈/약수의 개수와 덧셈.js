dee2fc8b898cf935d9c28f6c94983e4de40271f0
lkj1313 <108466769+lkj1313@users.noreply.github.com>
2025-03-26 18:00:57 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 31.48 ms, Memory: 36.3 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -1,18 +1,17 @@
 function solution(left, right) {
     var answer = 0;
     for(let i = left; i<=right; i++){
-      let divisorCount = 0;
-        for(let j =1 ; j<=i; j++){
-            if(i%j===0){
-               divisorCount++
+        let count =0;
+        for(let k = 1; k<=i; k++){
+            if(i%k===0){
+                count++
             }
         }
-        if(divisorCount%2===0){
-            answer+= i  
+        if(count%2===0){
+            answer+=i
         } else {
-            answer -= i;
+            answer-=i
         }
     }
-    
     return answer;
 }
