e23711bc1028400fcbaf36702360e22d33f09231
lkj1313 <108466769+lkj1313@users.noreply.github.com>
2025-03-26 18:33:36 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -1,19 +1,4 @@
 function solution(s) {
-    var answer = true;
-
-    // 문자열의 길이가 4 또는 6이 아닌 경우 바로 false 반환
-    if (s.length !== 4 && s.length !== 6) {
-        return false;
-    }
-
-    // 문자열을 숫자로 변환하여 NaN이 있는지 검사
-    const changeArray = s.split('').map(item => Number(item));
-    const filtered = changeArray.filter(a => isNaN(a)); // NaN이 있는지 확인
-
-    // NaN이 하나라도 있으면 answer는 false
-    if (filtered.length > 0) {
-        answer = false;
-    }
-
-    return answer;
+    if (s.length !== 4 && s.length !== 6) return false;
+    return s.split('').every(i => i >= '0' && i <= '9');
 }
