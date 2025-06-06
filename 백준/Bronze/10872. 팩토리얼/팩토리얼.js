dd63baafb6785de36c420b72cb8ed39b2b89da25
lkj1313 <108466769+lkj1313@users.noreply.github.com>
2025-06-06 10:27:38 +0900

    [Bronze III] Title: 팩토리얼, Time: 92 ms, Memory: 9604 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath, "utf8").trim();
+const N = Number(input);
+
+function fac(n) {
+  let result = 1;
+  for (let i = 1; i <= n; i++) {
+    result *= i;
+  }
+  return result;
+}
+console.log(fac(N));
