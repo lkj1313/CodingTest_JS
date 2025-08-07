1afdc6085f494ecea3a23b1bf759f9cd76e16530
lkj1313 <108466769+lkj1313@users.noreply.github.com>
2025-08-07 15:14:45 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 96 ms, Memory: 9424 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.js
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.js
@@ -1,32 +1,26 @@
 const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
-const n = parseInt(input[0]);
-let groupWordCount = 0;
-
-for (let i = 1; i <= n; i++) {
+const N = input[0];
+let count = 0;
+for (let i = 1; i <= N; i++) {
   const word = input[i];
-  let isGroupWord = true;
-  const seen = [];
+  const seen = new Set();
+  let isGroup = true;
+  let prevChar = "";
 
-  for (let j = 0; j < word.length; j++) {
-    const char = word[j];
-
-    // 이미 등장한 문자인데, 이전 문자와 연속되지 않으면 그룹 단어가 아님
-    if (seen.includes(char) && word[j - 1] !== char) {
-      isGroupWord = false;
-      break;
+  for (const char of word) {
+    if (char !== prevChar) {
+      if (seen.has(char)) {
+        isGroup = false;
+        break;
+      } else {
+        seen.add(char);
+      }
     }
-
-    // 아직 등장하지 않은 문자라면 seen에 추가
-    if (!seen.includes(char)) {
-      seen.push(char);
-    }
+    prevChar = char;
   }
-
-  if (isGroupWord) {
-    groupWordCount++;
-  }
+  if (isGroup) count++;
 }
 
-console.log(groupWordCount);
+console.log(count);
