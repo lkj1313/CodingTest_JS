7a0c220c6d460dcdf64b2e80e215fbd5df2471cb
lkj1313 <108466769+lkj1313@users.noreply.github.com>
2025-08-07 10:48:43 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 88 ms, Memory: 9324 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.js
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.js
@@ -1,17 +1,15 @@
 const fs = require("fs");
-const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n").map(Number);
+const input = fs.readFileSync("/dev/stdin").toString().trim();
+const arr = input.split("\n").map(Number);
 
-const allStudents = Array.from({ length: 30 }, (_, i) => i + 1); // 1부터 30까지 배열 생성
+const all = new Array(31).fill(false);
 
-input.forEach((student) => {
-  const index = allStudents.indexOf(student);
-  if (index !== -1) {
-    allStudents.splice(index, 1); // 제출한 학생을 배열에서 제거
+for (const num of arr) {
+  all[num] = true;
+}
+
+for (let i = 1; i <= 30; i++) {
+  if (!all[i]) {
+    console.log(i);
   }
-});
-
-// 남은 두 학생 번호를 오름차순으로 정렬한 후 출력
-allStudents.sort((a, b) => a - b);
-
-console.log(allStudents[0]);
-console.log(allStudents[1]);
+}
