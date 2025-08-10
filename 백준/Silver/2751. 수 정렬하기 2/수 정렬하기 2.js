3e49418e619f533e09a2848f9ddd476f61b62899
lkj1313 <108466769+lkj1313@users.noreply.github.com>
2025-08-10 16:10:59 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1232 ms, Memory: 219332 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n")
+  .map(Number);
+
+const arr = input.slice(1);
+
+arr.sort((a, b) => a - b);
+
+console.log(arr.join("\n"));
