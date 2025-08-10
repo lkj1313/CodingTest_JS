f8d6d6cabe11d9468565e2a90b6e2c66bf465d89
lkj1313 <108466769+lkj1313@users.noreply.github.com>
2025-08-10 16:53:48 +0900

    [Silver IV] Title: 수 찾기, Time: 264 ms, Memory: 48012 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.js
+++ 백준/Silver/1920. 수 찾기/수 찾기.js
@@ -2,11 +2,14 @@ const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
 const N = Number(input[0]);
-const two = input[1].split(" ").map(Number);
+const An = new Set(input[1].split(" ").map(Number));
 
 const M = Number(input[2]);
-const four = input[3].split(" ").map(Number);
+const Mn = input[3].split(" ").map(Number);
 
-const set = new Set(two); // 기준 집합
-const answer = four.map((num) => (set.has(num) ? 1 : 0));
-console.log(answer.join("\n"));
+let result = "";
+for (let i = 0; i < M; i++) {
+  result += (An.has(Mn[i]) ? 1 : 0) + "\n";
+}
+
+console.log(result);
