7806e9a7a64d27fdfb4891bfa33299c34b07a586
lkj1313 <108466769+lkj1313@users.noreply.github.com>
2025-08-13 11:03:49 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 912 ms, Memory: 165484 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.js
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.js
@@ -1,15 +1,25 @@
 const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
-const N = input[0];
-const cards = input[1].split(" ").map(Number);
-const M = input[2];
-const targets = input[3].split(" ").map(Number);
+const N = Number(input[0]);
 
-const cardCount = {};
-for (let num of cards) {
-  cardCount[num] = (cardCount[num] || 0) + 1;
+const have = input[1].split(" ").map(Number);
+have.sort((a, b) => a - b);
+
+const M = Number(input[2]);
+
+const mustHave = input[3].split(" ").map(Number);
+
+const freq = new Map();
+
+for (const v of have) {
+  if (freq.has(v)) {
+    freq.set(v, freq.get(v) + 1);
+  } else {
+    freq.set(v, 1);
+  }
 }
 
-const answer = targets.map((n) => cardCount[n] || 0).join(" ");
-console.log(answer);
+const res = mustHave.map((v) => freq.get(v) || 0);
+
+console.log(res.join(" "));
