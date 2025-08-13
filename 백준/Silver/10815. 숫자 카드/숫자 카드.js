9569e41f528273049f0479d5c56fdffe9739dce9
lkj1313 <108466769+lkj1313@users.noreply.github.com>
2025-08-13 11:32:53 +0900

    [Silver V] Title: 숫자 카드, Time: 612 ms, Memory: 147972 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.js
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.js
@@ -1,9 +1,13 @@
 const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
 const N = Number(input[0]);
-const Numbers = input[1].split(" ").map(Number);
+
+const have = new Set(input[1].split(" ").map(Number));
+
 const M = Number(input[2]);
-const Ms = input[3].split(" ").map(Number);
-const NumberSet = new Set(Numbers);
-const result = Ms.map((number) => (NumberSet.has(number) ? 1 : 0));
+
+const MustHave = input[3].split(" ").map(Number);
+
+const result = MustHave.map((num) => (have.has(num) ? 1 : 0));
 console.log(result.join(" "));
