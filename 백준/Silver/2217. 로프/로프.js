6b13765395de0994efd786e6ccdcbf875307e81e
lkj1313 <108466769+lkj1313@users.noreply.github.com>
2025-08-16 14:12:12 +0900

    [Silver IV] Title: 로프, Time: 220 ms, Memory: 20744 KB -BaekjoonHub

--- 백준/Silver/2217. 로프/로프.js
+++ 백준/Silver/2217. 로프/로프.js
@@ -1,17 +1,16 @@
 const fs = require("fs");
-const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-const input = fs.readFileSync(filePath, "utf8").trim().split("\n");
-const n = Number(input[0]);
-const lopes = input.slice(1).map(Number);
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
-const sorted = lopes.sort((a, b) => a - b);
+const N = +input[0];
 
-let sum = 0;
+const arr = input.slice(1).map(Number);
 
-for (let i = 0; i < n; i++) {
-  if (sorted[i] * (n - i) > sum) {
-    sum = sorted[i] * (n - i);
-  }
+arr.sort((a, b) => a - b);
+
+let max = 0;
+
+for (let i = 0; i < N; i++) {
+  max = Math.max(max, arr[i] * (N - i));
 }
 
-console.log(sum);
+console.log(max);
