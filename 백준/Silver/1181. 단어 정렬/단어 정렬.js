efb89170324368f7f3b1a58760f4a36cf795c583
lkj1313 <108466769+lkj1313@users.noreply.github.com>
2025-08-17 18:10:40 +0900

    [Silver V] Title: 단어 정렬, Time: 220 ms, Memory: 20124 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.js
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.js
@@ -1,13 +1,18 @@
 const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
-const num = input[0];
-const words = [...new Set(input.slice(1))];
 
-words.sort((a, b) => {
-  if (a.length === b.length) {
+const T = +input[0];
+
+const arr = input.slice(1);
+
+const uniquedArr = [...new Set(arr)];
+
+uniquedArr.sort((a, b) => {
+  if (a.length !== b.length) {
+    return a.length - b.length;
+  } else {
     return a.localeCompare(b);
   }
-  return a.length - b.length;
 });
 
-console.log(words.join("\n"));
+console.log(uniquedArr.join("\n"));
