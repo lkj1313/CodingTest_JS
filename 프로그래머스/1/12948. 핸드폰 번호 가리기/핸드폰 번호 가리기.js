37bc8086c4a4df11dc5633a61ab87d4e7323dacb
lkj1313 <108466769+lkj1313@users.noreply.github.com>
2025-12-27 14:31:20 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,6 +1,5 @@
 function solution(phone_number) {
     var answer = '';
-    const last4 = phone_number.slice(-4);
-    const start = '*'.repeat(phone_number.length-4);
-    return start+last4
+    const ㅁㅁ = '*'.repeat(phone_number.length-4) + phone_number.slice(-4)
+    return ㅁㅁ;
 }
