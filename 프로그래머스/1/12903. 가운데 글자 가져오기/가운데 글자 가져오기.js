69dbaa8168599e80e562d0b90d5ed4e192f9ddc8
lkj1313 <108466769+lkj1313@users.noreply.github.com>
2025-12-27 14:42:31 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.05 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -1,11 +1,13 @@
 function solution(s) {
     var answer = '';
-    if(s.length%2===0){ // 짝수개일때
-       
-        answer = s[(s.length/2)-1]+s[s.length/2]
-    } else { // 홀수개일때
-        const ceil = Math.floor(s.length/2)
-        answer = s[ceil]
-    }
+    const divide = s.length/2
+    if(Number.isInteger(divide))
+        //정수 ㅡ> 짝수
+         {
+             answer = s[divide-1]+s[divide]
+        
+     } else {
+         answer = s[Math.trunc(divide)]
+     }
     return answer;
 }
