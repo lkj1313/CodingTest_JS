b4b6874258c4c4838f652a4a2988f354ad59feec
lkj1313 <108466769+lkj1313@users.noreply.github.com>
2025-12-30 16:06:30 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 13.70 ms, Memory: 83.5 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,11 +1,12 @@
 function solution(arr)
 {
-    let answer = []
-   for(let i =0; i<arr.length; i++){
-       if(i===0|| arr[i]!==arr[i-1]){
+    const answer = []
+   for(let i=0; i<arr.length; i++){
+       if(arr[i]!==arr[i-1]){
            answer.push(arr[i])
        }
    }
-    
-    return answer;
+   return answer
 }
+
+
