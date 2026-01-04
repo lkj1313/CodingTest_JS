a21d43e7a9e8819ee6bb8c39472f6e716a3ab354
lkj1313 <108466769+lkj1313@users.noreply.github.com>
2026-01-04 12:59:32 +0900

    [level 1] Title: 삼총사, Time: 0.14 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.js
+++ 프로그래머스/1/131705. 삼총사/삼총사.js
@@ -1,13 +1,13 @@
 function solution(number) {
-     let count=0;
-    for(let i=0; i< number.length-2; i++){
-        for(let k=i+1; k<number.length-1; k++){
-            for(let j=k+1; j<number.length; j++){
-                if(number[i]+number[k]+number[j]===0){
-                    count++
+    var answer = 0;
+    for(let i=0; i<number.length; i++){
+        for(let j = i+1; j<number.length   ;j++){
+            for(let k = j+1; k<number.length; k++){
+                if(number[i]+number[j]+number[k]===0){
+                    answer++
                 }
             }
         }
     }
-    return count
+    return answer;
 }
