73e51ba925352534ed52217bbc2f883004a8fdf0
lkj1313 <108466769+lkj1313@users.noreply.github.com>
2026-01-04 13:18:50 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.20 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -1,18 +1,22 @@
 function solution(s) {
-    var array = [];
-    const splited_s = s.split(' ')
-    for(let i=0; i<splited_s.length; i++){
-        let 짝홀수된 = splited_s[i].split('').map((w,k)=>{
-            if(k%2===0){
-               return w.toUpperCase();
-            } else{
-                return w.toLowerCase();
-            }
-        })
-        const 조인 = 짝홀수된.join('')
-        array.push(조인);
+    let words = s.split(' ');
+    
+    for (let i = 0; i < words.length; i++) {
         
+        let chars = words[i].split(''); 
+        
+        for (let j = 0; j < chars.length; j++) {
+           
+            if (j % 2 === 0) {
+                chars[j] = chars[j].toUpperCase();
+            } else {
+                chars[j] = chars[j].toLowerCase();
+            }
+        }
+        
+      
+        words[i] = chars.join('');
     }
-    let answer = array.join(' ')
-    return answer;
+    
+    return words.join(' ');
 }
