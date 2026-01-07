3f7eb99b925133b9d28cb0daeb47e922f34c5eb1
lkj1313 <108466769+lkj1313@users.noreply.github.com>
2026-01-07 21:07:40 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.20 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -1,8 +1,11 @@
 function solution(food) {
-   let left = ''
-   for(i=1; i<food.length ;i++){
-       const count = Math.floor(food[i]/2);
-       left+=String(i).repeat(count)
-  }
-    return left + '0' + [...left].reverse().join('')
+    var answer = '';
+    for(let i=1; i<food.length ;i++){
+        if(food[i]<2) continue;
+        const number = Math.floor(food[i]/2);
+        answer+= String(i).repeat(number);
+    }
+    const reversedAnswer = [...answer].reverse().join('');
+    
+    return answer+'0'+reversedAnswer;
 }
