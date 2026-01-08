d98c3d3fa48d73eaeadda5215e2f0ddf3e29a8c2
lkj1313 <108466769+lkj1313@users.noreply.github.com>
2026-01-08 19:03:58 +0900

    [Bronze V] Title: 시험 성적, Time: 96 ms, Memory: 9340 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.js
+++ 백준/Bronze/9498. 시험 성적/시험 성적.js
@@ -1,17 +1,14 @@
 const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim();
 
-// 입력을 받아서 처리
-const input = fs.readFileSync("/dev/stdin").toString();
-
-const A = +input;
-
-if (A >= 90) {
+const score = Number(input);
+if (score >= 90) {
   console.log("A");
-} else if (A >= 80) {
+} else if (score >= 80) {
   console.log("B");
-} else if (A >= 70) {
+} else if (score >= 70) {
   console.log("C");
-} else if (A >= 60) {
+} else if (score >= 60) {
   console.log("D");
 } else {
   console.log("F");
