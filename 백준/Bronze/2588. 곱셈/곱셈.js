b28b49721a003613d67997314cc9672549a16868
lkj1313 <108466769+lkj1313@users.noreply.github.com>
2026-01-08 18:38:41 +0900

    [Bronze III] Title: 곱셈, Time: 88 ms, Memory: 9580 KB -BaekjoonHub

--- 백준/Bronze/2588. 곱셈/곱셈.js
+++ 백준/Bronze/2588. 곱셈/곱셈.js
@@ -1,14 +1,15 @@
 const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
-// 입력을 받아서 처리
-const input = fs.readFileSync("/dev/stdin").toString().split("\n");
-const A = +input[0];
+const A = input[0];
 const B = input[1];
-const B1 = parseInt(B[2]);
-const B2 = parseInt(B[1]);
-const B3 = parseInt(B[0]);
+const answer = [];
 
-console.log(A * B1);
-console.log(A * B2);
-console.log(A * B3);
-console.log(A * parseInt(B));
+for (let i = 2; i >= 0; i--) {
+  answer.push(Number(A) * Number(B[i]));
+}
+const totalSum = Number(A) * Number(B);
+
+answer.push(totalSum);
+
+console.log(answer.join("\n"));
