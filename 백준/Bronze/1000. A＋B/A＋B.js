1536d11e47bfac743dd3b7eb75a00d72ae8b8813
lkj1313 <108466769+lkj1313@users.noreply.github.com>
2026-01-08 18:17:30 +0900

    [Bronze V] Title: A+B, Time: 96 ms, Memory: 9348 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.js
+++ 백준/Bronze/1000. A＋B/A＋B.js
@@ -1,11 +1,7 @@
 const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split(" ");
 
-// 입력을 받아서 처리
-const input = fs.readFileSync("/dev/stdin").toString().split(" ");
+const A = +input[0];
+const B = +input[1];
 
-// A와 B를 정수로 변환
-const A = parseInt(input[0]);
-const B = parseInt(input[1]);
-
-// 각 연산 결과를 출력
 console.log(A + B);
