47e9bf15f186865e4fd7854f8c1f21a2d2c25d42
lkj1313 <108466769+lkj1313@users.noreply.github.com>
2026-01-08 20:15:17 +0900

    [Bronze V] Title: 구구단, Time: 88 ms, Memory: 9344 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.js
+++ 백준/Bronze/2739. 구구단/구구단.js
@@ -1,7 +1,9 @@
 const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString().trim();
 
-const A = +input;
-for(let i=1; i<=9; i++){
-  console.log(`${A} * ${i} = ${A*i}`)
+const N = +input;
+const answer = [];
+
+for (let i = 1; i <= 9; i++) {
+  console.log(`${N} * ${i} = ${N * i}`);
 }
