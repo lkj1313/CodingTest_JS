b097fd059d53dd15155b4664493536e75a5fbf11
lkj1313 <108466769+lkj1313@users.noreply.github.com>
2026-01-12 14:54:57 +0900

    [Silver II] Title: 랜선 자르기, Time: 168 ms, Memory: 13568 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.js
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.js
@@ -2,20 +2,25 @@ const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
 const [K, N] = input[0].split(" ").map(Number);
-const cables = input.slice(1).map(Number);
-let start = 1;
-let end = Math.max(...cables);
-let result;
 
-while (start <= end) {
-  const mid = Math.floor((start + end) / 2);
-  let count = cables.reduce((a, cable) => a + Math.floor(cable / mid), 0);
-  if (count >= N) {
-    result = mid;
-    start = mid + 1;
+const lanse = input.slice(1).map(Number);
+
+let low = 1;
+let high = Math.max(...lanse);
+let answer = 0;
+while (high >= low) {
+  let mid = Math.floor((low + high) / 2);
+  let total = 0; // 랜선갯수
+  for (let x of lanse) {
+    total += Math.floor(x / mid);
+  }
+
+  if (total >= N) {
+    answer = mid;
+    low = mid + 1;
   } else {
-    end = mid - 1;
+    high = mid - 1;
   }
 }
 
-console.log(result);
+console.log(answer);
