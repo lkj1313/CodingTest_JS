c2a7ef6288f524b95eaf0e0faa89e869e7aae22d
lkj1313 <108466769+lkj1313@users.noreply.github.com>
2026-01-27 18:16:10 +0900

    [level 1] Title: 기사단원의 무기, Time: 104.99 ms, Memory: 35.4 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
@@ -1,13 +1,26 @@
 function solution(number, limit, power) {
-    var result = 0;
-    for(let i=1; i<=number ;i++){
-        let count = 0;
-        for(let j=1; j*j<=i;j++){
-            if(i%j===0) {count++
-                        if(i!==j*j) count++}
-           
+    let result = 0;
+    for(let i=1; i<=number; i++){
+        let divisor = countDivisiors(i)
+        if(divisor>limit){
+            result+=power;
+        } else{
+            result+=divisor;
         }
-        count>limit ? result+=power : result+=count
+        
     }
     return result;
+}
+
+function countDivisiors(number){
+    let count = 0;
+    for(let i =1; i*i<=number; i++){
+        if(number%i===0){
+        if(i*i===number){
+            count++
+        } else{
+            count = count+2
+        }
+    }}
+    return count;
 }
