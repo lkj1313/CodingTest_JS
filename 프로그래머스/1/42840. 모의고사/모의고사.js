82a3022db5044c84ec34808948237ade773f2457
lkj1313 <108466769+lkj1313@users.noreply.github.com>
2026-01-27 19:36:41 +0900

    [level 1] Title: 모의고사, Time: 4.15 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,22 +1,26 @@
 function solution(answers) {
-    var answer = [];
-    const patterns =[
-                     [1, 2, 3, 4, 5],
-                     [2, 1, 2, 3, 2, 4, 2, 5],
-                     [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
-                    ]
-    const scores = [0,0,0]
-  answers.forEach((answer, i) => {
-  patterns.forEach((pattern, index) => {
-    if (answer === pattern[i % pattern.length]) {
-      scores[index]++;
+    const patterns = [
+        [1, 2, 3, 4, 5],
+        [2, 1, 2, 3, 2, 4, 2, 5],
+        [3, 3, 1, 1, 2, 2, 4, 4, 5, 5],
+    ];
+    const scores = [0,0,0];
+    for(let i =0; i<answers.length; i++){
+        for(let j=0; j<3; j++){
+            if(answers[i]===patterns[j][i % patterns[j].length]){
+                scores[j] = scores[j]+1;
+            }
+        }
     }
-  });
-});
-    const maxScore = Math.max(...scores);
-
+    const max = Math.max(...scores);
+    const answer = [];
+    scores.forEach((num,idx)=>{
+        if(num===max){
+            answer.push(idx+1);
+        }
+    })
+    answer.sort((a,b)=>a-b);
     
-    return scores.map((score,i)=>{
-        return score===maxScore ? i+1 : null
-    }).filter((v) => v !== null);
+    return answer;
+    
 }
