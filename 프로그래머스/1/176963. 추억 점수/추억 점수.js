2d9bef12b08e0124e2305085786a75bd6d399259
lkj1313 <108466769+lkj1313@users.noreply.github.com>
2026-01-27 17:27:53 +0900

    [level 1] Title: 추억 점수, Time: 2.19 ms, Memory: 34.2 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.js
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.js
@@ -1,17 +1,16 @@
 function solution(name, yearning, photo) {
-  var answer = [];
-  const map = new Map();
-  for (let i = 0; i < name.length; i++) {
-    map.set(name[i], yearning[i]);
-  }
-
-  for (let i = 0; i < photo.length; i++) {
-    let sum = 0;
-
-    for (let j = 0; j < photo[i].length; j++) {
-      sum += map.get(photo[i][j]) || 0;
+    var answer = [];
+  
+    const map = new Map();
+    for(let i =0; i<name.length; i++){
+        map.set(name[i],yearning[i]);
     }
-    answer.push(sum);
-  }
-  return answer;
+    for(const arr of photo){
+        let currentScore = 0;
+        for(const name of arr){
+           if(map.has(name)) currentScore +=  map.get(name);
+        }
+        answer.push(currentScore);
+    }
+    return answer
 }
