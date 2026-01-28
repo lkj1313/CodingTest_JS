477be47624d3ad1b9894652a8da9c15025c03af6
lkj1313 <108466769+lkj1313@users.noreply.github.com>
2026-01-28 14:16:42 +0900

    [Silver IV] Title: 문자열 집합, Time: 140 ms, Memory: 32188 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.js
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.js
@@ -1,17 +1,15 @@
 const fs = require("fs");
-const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-const input = fs.readFileSync(filePath, "utf8").trim().split("\n");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
 const [N, M] = input[0].split(" ").map(Number);
+const arr = input.slice(1);
+const S = arr.slice(0, N);
+const Marr = arr.slice(N);
 
-const map = new Map();
+const set = new Set(S);
 let count = 0;
-for (let i = 1; i <= N; i++) {
-  map.set(input[i], 0);
-}
-
-for (let i = N + 1; i <= M + N + 1; i++) {
-  if (map.has(input[i])) count++;
+for (let i = 0; i < Marr.length; i++) {
+  if (set.has(Marr[i])) count++;
 }
 
 console.log(count);
