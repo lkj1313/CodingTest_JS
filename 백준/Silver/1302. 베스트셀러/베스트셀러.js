db145b1327a706a608eb08b08cca63ae6563acda
lkj1313 <108466769+lkj1313@users.noreply.github.com>
2026-01-28 16:35:33 +0900

    [Silver IV] Title: 베스트셀러, Time: 100 ms, Memory: 9596 KB -BaekjoonHub

--- 백준/Silver/1302. 베스트셀러/베스트셀러.js
+++ 백준/Silver/1302. 베스트셀러/베스트셀러.js
@@ -1,26 +1,27 @@
 const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
-const N = Number(input[0]);
-const books = input.slice(1);
+
+const N = +input[0];
+const arr = input.slice(1);
 const map = new Map();
 
 for (let i = 0; i < N; i++) {
-  if (map.has(books[i])) {
-    map.set(books[i], map.get(books[i]) + 1);
-  } else {
-    map.set(books[i], 1);
-  }
+  const name = arr[i];
+  map.set(name, (map.get(name) || 0) + 1);
 }
-let maxCount = 0;
-let bestSeller = "";
-for (const [title, count] of map) {
-  if (count > maxCount) {
-    maxCount = count;
-    bestSeller = title;
-  }
-  if (count === maxCount && title < bestSeller) {
-    bestSeller = title;
+
+const newArr = [...map];
+
+let max = 0;
+
+for (let i = 0; i < newArr.length; i++) {
+  if (newArr[i][1] > max) {
+    max = newArr[i][1];
   }
 }
 
-console.log(bestSeller);
+const filtered = newArr.filter((a) => a[1] === max);
+
+filtered.sort((a, b) => a[0].localeCompare(b[0]));
+
+console.log(filtered[0][0]);
