c7595e2ed95e0b61e89df739a22c3d560ce11170
lkj1313 <108466769+lkj1313@users.noreply.github.com>
2026-01-28 13:04:30 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 60.27 ms, Memory: 65.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,17 +1,22 @@
 function solution(participant, completion) {
-    const map = new Map()
-    //참가자 목록을 해시맵에 기록 (이름->개수)
-    for (const name of participant){
-        map.set(name, (map.get(name) || 0 )+1 )
-    }
-    // 해시맵에서 완주한사람들 밸류뺌
-    for(const name of completion){
-        map.set(name, map.get(name)-1)
-    }
- 
-    for(const [name,count] of map){
-        if(count>0){
-            return name;
+    const map = new Map();
+    
+    for(let i =0; i<participant.length; i++){
+        if(map.has(participant[i])){
+            map.set(participant[i], map.get(participant[i])+1);
+            
+        } else{
+            map.set(participant[i],1);
         }
     }
+    for(let i=0; i<completion.length ; i++){
+        map.set(completion[i], map.get(completion[i])-1);
+    }
+    
+    for([par,count] of map){
+        if(count>0){
+            return par;
+        }
+    }
+ 
 }
