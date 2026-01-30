f983b9a1c8a35acb1d04373d66854fbb7d9f7648
lkj1313 <108466769+lkj1313@users.noreply.github.com>
2026-01-30 20:55:46 +0900

    [level 2] Title: 타겟 넘버, Time: 37.93 ms, Memory: 36.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -0,0 +1,17 @@
+function solution(numbers, target) {
+     let count  = 0;
+    
+    function dfs(idx, sum){
+        if(idx===numbers.length){
+            if(target===sum) count++;
+            return;
+        }
+        dfs(idx+1, sum+numbers[idx]);
+        dfs(idx+1, sum-numbers[idx])
+    }
+    
+    dfs(0,0);
+    return count;
+    
+    
+}
