5bba2976351a76ae5675cf48cd53514672299086
lkj1313 <108466769+lkj1313@users.noreply.github.com>
2026-01-31 14:37:32 +0900

    [Silver II] Title: 주식, Time: 2480 ms, Memory: 505256 KB -BaekjoonHub

--- 백준/Silver/11501. 주식/주식.js
+++ 백준/Silver/11501. 주식/주식.js
@@ -1,27 +1,22 @@
 const fs = require("fs");
-const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-let input = fs.readFileSync(filePath).toString().trim().split("\n");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
-const T = Number(input[0]);
-let line = 1;
-let answer = [];
-
-for (let t = 0; t < T; t++) {
-  const n = Number(input[line]);
-  const prices = input[line + 1].split(" ").map(Number);
-
-  let maxProfit = 0;
-  let maxPrice = 0;
-  for (let i = n - 1; i >= 0; i--) {
-    // 오타 수정: i--
-    const price = prices[i];
-    if (price > maxPrice) {
-      maxPrice = price;
+let line = 0;
+const N = +input[line++];
+let answerArr = [];
+for (let i = 0; i < N; i++) {
+  const T = +input[line++];
+  const arr = input[line++].split(" ").map(Number);
+  const length = arr.length;
+  let max = 0;
+  let answer = 0;
+  for (let k = length - 1; k >= 0; k--) {
+    if (arr[k] > max) {
+      max = arr[k];
     } else {
-      maxProfit += maxPrice - price;
+      answer += max - arr[k];
     }
   }
-  // answer.push(maxProfit); // 모든 결과를 모아서 출력할 경우 사용
-  line = line + 2;
-  console.log(maxProfit); // 수정된 부분: profit -> maxProfit
+  answerArr.push(answer);
 }
+console.log(answerArr.join("\n"));
