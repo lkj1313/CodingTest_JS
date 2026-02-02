6aabd43b90b4260d336cae676b9970c115e32df0
lkj1313 <108466769+lkj1313@users.noreply.github.com>
2026-02-02 11:23:19 +0900

    [Silver III] Title: 1로 만들기, Time: 164 ms, Memory: 17464 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.js
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.js
@@ -1,21 +1,17 @@
 const fs = require("fs");
-const input = Number(fs.readFileSync("/dev/stdin").toString().trim());
+const input = fs.readFileSync("/dev/stdin").toString().trim();
 
-const dp = new Array(input + 1).fill(0); // dp[i] = i를 1로 만들기 위한 최소 연산 횟수
-
-for (let i = 2; i <= input; i++) {
-  // 기본 연산: 1을 뺀 경우
+let X = +input;
+const dp = new Array(X + 1).fill(0);
+dp[2] = 1;
+for (let i = 3; i <= X; i++) {
   dp[i] = dp[i - 1] + 1;
-
-  // 2로 나눌 수 있는 경우
   if (i % 2 === 0) {
-    dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+    dp[i] = Math.min(dp[i / 2] + 1, dp[i]);
   }
-
-  // 3으로 나눌 수 있는 경우
   if (i % 3 === 0) {
-    dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+    dp[i] = Math.min(dp[i / 3] + 1, dp[i]);
   }
 }
 
-console.log(dp[input]); // 결과 출력
+console.log(dp[X]);
