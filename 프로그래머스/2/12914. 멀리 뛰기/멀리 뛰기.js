c2ff64ea1b20478ba4460063bc2c0557976fab68
lkj1313 <108466769+lkj1313@users.noreply.github.com>
2026-02-04 17:53:59 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.24 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -0,0 +1,14 @@
+function solution(n) {
+    
+    
+    const dp = new Array(n+1).fill(0);
+    dp[1] = 1;
+    dp[2] = 2;
+    dp[3] = 3;
+    for(let i =4; i<=n; i++){
+        dp[i] = (dp[i-1] + dp[i-2])%1234567;
+        
+    }
+    return dp[n] 
+    
+}
