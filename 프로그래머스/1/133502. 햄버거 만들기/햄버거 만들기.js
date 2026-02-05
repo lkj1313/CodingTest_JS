420d01b945f0ae7cdf3c0d994a1eaecba042aada
lkj1313 <108466769+lkj1313@users.noreply.github.com>
2026-02-05 17:30:25 +0900

    [level 1] Title: 햄버거 만들기, Time: 49.63 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.js
@@ -0,0 +1,18 @@
+function solution(ingredient) {
+    const stack = [];
+    let result = 0;
+    for(let i =0; i<ingredient.length ;i++){
+        stack.push(ingredient[i]);
+        if(stack.length>=4){
+            if(stack[stack.length-1]===1&&stack[stack.length-2]===3&&stack[stack.length-3]==2&&stack[stack.length-4]===1){
+                result++;
+                stack.pop();
+                stack.pop();
+                stack.pop();
+                stack.pop();
+                
+            }
+        }
+    }
+    return result;
+}
