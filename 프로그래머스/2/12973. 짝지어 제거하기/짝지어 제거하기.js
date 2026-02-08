0330a545c66f669b0574b69a6696a9deb53a03f6
lkj1313 <108466769+lkj1313@users.noreply.github.com>
2026-02-08 11:20:23 +0900

    [level 2] Title: 짝지어 제거하기, Time: 55.90 ms, Memory: 51 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -0,0 +1,13 @@
+function solution(s){
+    const stack = [];
+    for(let i=0; i<s.length  ;i++){
+        if(stack.length>0 && stack[stack.length-1]===s[i]){
+            stack.pop();
+        } else{
+            stack.push(s[i])
+        }
+        
+    }
+    if(stack.length>0) return 0
+    else return 1;
+}
