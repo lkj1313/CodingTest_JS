aee25e6f4c7c2b189177ee0f6e0a9b4cf73c7175
lkj1313 <108466769+lkj1313@users.noreply.github.com>
2026-02-21 07:55:28 +0900

    [level 1] Title: 모의고사, Time: 5.14 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,26 +1,26 @@
 function solution(answers) {
-    const patterns = [
-        [1, 2, 3, 4, 5],
-        [2, 1, 2, 3, 2, 4, 2, 5],
-        [3, 3, 1, 1, 2, 2, 4, 4, 5, 5],
-    ];
-    const scores = [0,0,0];
-    for(let i =0; i<answers.length; i++){
-        for(let j=0; j<3; j++){
-            if(answers[i]===patterns[j][i % patterns[j].length]){
-                scores[j] = scores[j]+1;
-            }
-        }
-    }
-    const max = Math.max(...scores);
-    const answer = [];
-    scores.forEach((num,idx)=>{
-        if(num===max){
-            answer.push(idx+1);
-        }
-    })
-    answer.sort((a,b)=>a-b);
+  const one = [1,2,3,4,5] // 5
+  const two = [2,1,2,3,2,4,2,5] // 8
+  const three = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5] // 10
+  
+  let count = new Array(3).fill(0);
     
-    return answer;
+  for(let i=0; i<answers.length; i++){
+      if(answers[i] === one[i%5]) count[0]++;
+      if(answers[i] === two[i%8]) count[1]++;
+      if(answers[i] === three[i%10]) count[2]++;
+      
+  }
+  const max = Math.max(...count);
+  const answer = count.map((n,i)=>{
+      if(max!==n){
+          return -1;
+      } else{
+          return i+1;
+      }
+  }).filter((n)=>n!==-1);
+  answer.sort((a,b)=>a-b);
+    
+  return answer
     
 }
