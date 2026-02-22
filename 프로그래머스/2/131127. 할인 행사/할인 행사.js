c85694f04515ac5d85a01687b9eb0a52b41f4106
lkj1313 <108466769+lkj1313@users.noreply.github.com>
2026-02-22 11:52:52 +0900

    [level 2] Title: 할인 행사, Time: 97.24 ms, Memory: 41.3 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.js
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.js
@@ -1,27 +1,30 @@
 function solution(want, number, discount) {
- 
-    const wantMap = new Map();
-    for(let i=0; i<want.length ;i++){
-        wantMap.set(want[i], number[i]);
+  const wantMap = new Map();
+  let count = 0;
+
+  for (let i = 0; i < want.length; i++) {
+    wantMap.set(want[i], number[i]);
+  }
+
+  for (let i = 0; i <= discount.length - 10; i++) { // <= 로 수정
+    let flag = true;
+    const discountedArr = discount.slice(i, i + 10);
+    const discountedMap = new Map();
+
+    for (let j = 0; j < 10; j++) {
+      const item = discountedArr[j];
+      discountedMap.set(item, (discountedMap.get(item) || 0) + 1); // 수정
     }
-    let count = 0;
-    for(let i=0; i<=discount.length-10 ;i++){
-        const discountedMap = new Map();
-        let flag = true;
-        const arr = discount.slice(i, 10+i)
-        for(let i=0; i<arr.length; i++){
-            discountedMap.set(arr[i], (discountedMap.get(arr[i]) || 0 )+1)
-        }
-        for(const [key,value] of wantMap){
-            if(discountedMap.get(key) !== value){
-                flag=false;
-                break;
-            }
-        }
-        if(flag) count++;
+
+    for (const [key, value] of wantMap.entries()) {
+      if ((discountedMap.get(key) || 0) < value) { // 안전하게
+        flag = false;
+        break;
+      }
     }
-    
-    
-    
-    return count;
+
+    if (flag) count++;
+  }
+
+  return count;
 }
