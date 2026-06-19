b070db6e08c4cf09e77b68d44c6c6e46aaf3a5cd
lkj1313 <108466769+lkj1313@users.noreply.github.com>
2026-06-19 16:09:34 +0900

    [level 2] Title: 괄호 회전하기, Time: 7.02 ms, Memory: 51.4 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -1,33 +1,34 @@
-function isValid(str){
-    const stack = [];
-    const pair = {
-        ')' : '(',
-        ']' : '[',
-        '}' : '{'
-    };
-    for(const char of str){
-        if(char==='(' || char==='[' || char==='{'){
-            stack.push(char)
-        } else{
-            if(stack.length===0) return false;
-            const top = stack.pop();
-            if(top===pair[char]){
-                continue;
-            } else{
-                return false;
-            }
+function solution(s) {
+    let count = 0;
+    for(let i=0;  i<s.length; i++){
+        const str = s.slice(i) + s.slice(0,i);
+        if(isValid(str)){
+            count++;
         }
     }
     
-    return stack.length===0;
-}
-
-
-function solution(s) {
-    let count = 0;
-    for(let i=0; i<s.length; i++){
-        const str = s.slice(i) + s.slice(0,i);
-        if(isValid(str)) count++;
+    
+    function isValid(str){
+        const stack = [];
+        const pair = {
+            ')': '(',
+            ']': '[',
+            '}': '{'
+        };
+        for(let i=0; i<str.length; i++){
+            if(str[i] === '(' || str[i] === '{' || str[i] === '['){
+                stack.push(str[i]);
+            } else{
+                if(stack.length===0){
+                    return false;
+                }
+                if(stack.pop()!==pair[str[i]]){
+                    return false;
+                }
+            }
+        }
+        return stack.length ===0; 
     }
     return count;
+        
 }
